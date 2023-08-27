07bfa55f996ccba5a8fe0c2626e00eb827085e59
dvcuong97 <cuonggdv08@gmail.com>
2023-08-27 10:37:50 +0700

    nộp bài

---
+++ src/BaiTapChinh/Content.js
@@ -0,0 +1,212 @@
+import React, { Component } from "react";
+
+export default class Content extends Component {
+  render() {
+    return (
+      <section class="pt-4">
+        <div class="container px-lg-5">
+          <div class="row gx-lg-5">
+            <div class="col-4 mb-5">
+              <div
+                class="card bg-light border-0 h-100"
+                style={{ position: "relative" }}
+              >
+                <div
+                  class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0"
+                  // style={{display:"flex", flexDirection: }}
+                >
+                  <div
+                    class="feature bg-primary bg-gradient text-white rounded-lg mb-4 mt-n4"
+                    style={{
+                      display: "inline-flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      height: "4rem",
+                      width: "4rem",
+                      fontSize: "2rem",
+                      position: "absolute",
+                      top: "-10px",
+                      left: "130px",
+                    }}
+                  >
+                    <i class="fab fa-500px"></i>
+                  </div>
+                  <h2 class="fs-4 fw-bold">Fresh new layout</h2>
+                  <p class="mb-0">
+                    With Bootstrap 5, we've created a fresh new layout for this
+                    template!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-4 mb-5">
+              <div
+                class="card bg-light border-0 h-100"
+                style={{ position: "relative" }}
+              >
+                <div
+                  class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0"
+                  // style={{display:"flex", flexDirection: }}
+                >
+                  <div
+                    class="feature bg-primary bg-gradient text-white rounded-lg mb-4 mt-n4"
+                    style={{
+                      display: "inline-flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      height: "4rem",
+                      width: "4rem",
+                      fontSize: "2rem",
+                      position: "absolute",
+                      top: "-10px",
+                      left: "130px",
+                    }}
+                  >
+                    <i class="fab fa-500px"></i>
+                  </div>
+                  <h2 class="fs-4 fw-bold">Fresh new layout</h2>
+                  <p class="mb-0">
+                    With Bootstrap 5, we've created a fresh new layout for this
+                    template!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-4 mb-5">
+              <div
+                class="card bg-light border-0 h-100"
+                style={{ position: "relative" }}
+              >
+                <div
+                  class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0"
+                  // style={{display:"flex", flexDirection: }}
+                >
+                  <div
+                    class="feature bg-primary bg-gradient text-white rounded-lg mb-4 mt-n4"
+                    style={{
+                      display: "inline-flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      height: "4rem",
+                      width: "4rem",
+                      fontSize: "2rem",
+                      position: "absolute",
+                      top: "-10px",
+                      left: "130px",
+                    }}
+                  >
+                    <i class="fab fa-500px"></i>
+                  </div>
+                  <h2 class="fs-4 fw-bold">Fresh new layout</h2>
+                  <p class="mb-0">
+                    With Bootstrap 5, we've created a fresh new layout for this
+                    template!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-4 mb-5">
+              <div
+                class="card bg-light border-0 h-100"
+                style={{ position: "relative" }}
+              >
+                <div
+                  class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0"
+                  // style={{display:"flex", flexDirection: }}
+                >
+                  <div
+                    class="feature bg-primary bg-gradient text-white rounded-lg mb-4 mt-n4"
+                    style={{
+                      display: "inline-flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      height: "4rem",
+                      width: "4rem",
+                      fontSize: "2rem",
+                      position: "absolute",
+                      top: "-10px",
+                      left: "130px",
+                    }}
+                  >
+                    <i class="fab fa-500px"></i>
+                  </div>
+                  <h2 class="fs-4 fw-bold">Fresh new layout</h2>
+                  <p class="mb-0">
+                    With Bootstrap 5, we've created a fresh new layout for this
+                    template!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-4 mb-5">
+              <div
+                class="card bg-light border-0 h-100"
+                style={{ position: "relative" }}
+              >
+                <div
+                  class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0"
+                  // style={{display:"flex", flexDirection: }}
+                >
+                  <div
+                    class="feature bg-primary bg-gradient text-white rounded-lg mb-4 mt-n4"
+                    style={{
+                      display: "inline-flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      height: "4rem",
+                      width: "4rem",
+                      fontSize: "2rem",
+                      position: "absolute",
+                      top: "-10px",
+                      left: "130px",
+                    }}
+                  >
+                    <i class="fab fa-500px"></i>
+                  </div>
+                  <h2 class="fs-4 fw-bold">Fresh new layout</h2>
+                  <p class="mb-0">
+                    With Bootstrap 5, we've created a fresh new layout for this
+                    template!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-4 mb-5">
+              <div
+                class="card bg-light border-0 h-100"
+                style={{ position: "relative" }}
+              >
+                <div
+                  class="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0"
+                  // style={{display:"flex", flexDirection: }}
+                >
+                  <div
+                    class="feature bg-primary bg-gradient text-white rounded-lg mb-4 mt-n4"
+                    style={{
+                      display: "inline-flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      height: "4rem",
+                      width: "4rem",
+                      fontSize: "2rem",
+                      position: "absolute",
+                      top: "-10px",
+                      left: "130px",
+                    }}
+                  >
+                    <i class="fab fa-500px"></i>
+                  </div>
+                  <h2 class="fs-4 fw-bold">Fresh new layout</h2>
+                  <p class="mb-0">
+                    With Bootstrap 5, we've created a fresh new layout for this
+                    template!
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
